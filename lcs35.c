d5ea7f491f834531f197a5f06fcf14f36a7416e5
Joshua Neuheisel <joshua@neuheisel.us>
2019-05-11 14:11:00 -0400

    added holder for square results

--- lcs35.c
+++ lcs35.c
@@ -9,11 +9,11 @@ int
 main (void)
 {
   int rv;
-  mpz_t n, z, w;
+  mpz_t n, z, w, w2;
   uint64_t t = T;
 
   /* Initializations */
-  mpz_inits (n, z, w, NULL);
+  mpz_inits (n, z, w, w2, NULL);
 
   rv = mpz_set_str (n, N, 10);
   CHECK_RV (rv, "failed to set n\n");
@@ -25,8 +25,8 @@ main (void)
   /* Get going */
   while (t-- > 0)
     {
-      mpz_mul (w, w, w);
-      mpz_mod (w, w, n);
+      mpz_mul (w2, w, w);
+      mpz_mod (w, w2, n);
     }
 
   /* Create and show the message */
@@ -37,7 +37,7 @@ main (void)
   printf ("\n");
 
   /* Clean up */
-  mpz_clears (n, z, w, NULL);
+  mpz_clears (n, z, w, w2, NULL);
 
   return EXIT_SUCCESS;
 }
