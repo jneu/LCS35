a35a768de3f21247a78e0415b9b0f8af541a5d9a
Joshua Neuheisel <joshua@neuheisel.us>
2019-05-11 08:46:31 -0400

    show ascii message

--- validate_challenge.c
+++ validate_challenge.c
@@ -115,6 +115,8 @@ void
 print_message (mpz_t * z, mpz_t * w)
 {
   mpz_t message;
+  size_t offset;
+  const uint8_t *raw_bytes;
 
   mpz_init (message);
 
@@ -122,6 +124,19 @@ print_message (mpz_t * z, mpz_t * w)
 
   printf ("message:\n");
   mpz_out_str (stdout, 10, message);
+  printf ("\n");
+
+  offset = mpz_size (message) * sizeof (mp_limb_t);
+  raw_bytes = (const uint8_t *) mpz_limbs_read (message);
+
+  while ((offset > 0) && (0 == raw_bytes[offset - 1]))
+    {
+      offset--;
+    }
+  while (offset > 0)
+    {
+      putchar ((int) raw_bytes[--offset]);
+    }
   printf ("\n");
 
   mpz_clear (message);
